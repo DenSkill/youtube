8d9a83de347da43965463e4bdc741e0b59413e49
jkfknbchdx <denskillhigh@mail.ru>
2023-02-24 22:53:47 +0500

    ??

--- venv/main.py
+++ venv/main.py
@@ -4,18 +4,10 @@ import os
 from googleapiclient.discovery import build
 import json
 
-# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
-
-# создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
-channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
-
-channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-
-print(json.dumps(channel, indent=2, ensure_ascii=False))
+channel = youtube.channels().list(id=channel_id, part='snippet,statistics,description,items,viewCount').execute()
 
 
 class Channel:
@@ -23,3 +15,9 @@ class Channel:
         self.id = id
 
     def print_info(self):
+        text = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        return json.loads(text)
+
+
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+vdud.print_info()
