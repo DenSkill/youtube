4795caac17952a3f619fe538ae3459979e77d983
jkfknbchdx <denskillhigh@mail.ru>
2023-02-25 13:15:06 +0500

    Вынес api key, ничего не поменялось

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,9 @@
-from config import api_key
 import os
 
 from googleapiclient.discovery import build
 import json
+
+api_key = 'AIzaSyB_zRuBrxjY-cBxVeGZPzn7JeEofSDpe5c'
 
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
