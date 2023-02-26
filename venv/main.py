1bd39bcb4aa01c0f0dbc432d10d09b9533128c4c
jkfknbchdx <denskillhigh@mail.ru>
2023-02-26 20:06:09 +0500

    В чем ошибка?

--- venv/main.py
+++ venv/main.py
@@ -3,22 +3,27 @@ import os
 from googleapiclient.discovery import build
 import json
 
-api_key = 'AIzaSyB_zRuBrxjY-cBxVeGZPzn7JeEofSDpe5c'
-
-youtube = build('youtube', 'v3', developerKey=api_key)
-channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'
-channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+from dotenv import load_dotenv
 
 
-class Channel:
-    def __init__(self, id):
-        self.id = id
+class Youtube:
+    def __init__(self, channel_id):
         self.channel_id = channel_id
+        load_dotenv()
+        api_key: str = os.getenv('YOUTUBE_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel_info = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.tittle = self.channel_info['items'][0]['snippet']['tittle']
+        self.description = self.channel_info['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/' + self.channel_id
+        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
+        data = self.tittle = self.description + self.url + self.subscriber_count + self.video_count + self.view_count
 
     def print_info(self):
-        text = json.dumps(self.channel_id, indent=2, ensure_ascii=False)
-        print(json.loads(text))
+        print(json.dumps(self.channel_info, indent=2, ensure_ascii=False))
 
 
-vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-vdud.print_info()
+y = Youtube('UCMCgOm8GZkHp8zJ6l7_hIuA')
+y.print_info()
