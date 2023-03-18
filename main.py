41ddc3e71365dbdf72a2e3c5172b12539df364b1
jkfknbchdx <denskillhigh@mail.ru>
2023-03-18 23:40:13 +0500

    try/except

--- main.py
+++ main.py
@@ -59,18 +59,27 @@ class Video:
     __API_KEY: str = os.getenv('api_key')
 
     def __init__(self, video_id):
-        self.__video_id = video_id
-        self._init_from_api()
+        self.video_id = video_id
+        try:
+            youtube = self.get_service()
+            self.video_response = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.video_response = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
 
-    def _init_from_api(self):
-        video_response = self.get_service().videos().list(part='snippet,statistics',
-                                                          id=self.__video_id
-                                                          ).execute()
-
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.__video_id}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+    def __str__(self):
+        try:
+            if self.title is not None:
+                return self.title
+            else:
+                raise TypeError
+        except TypeError:
+            return f'Невозможно получить данные по этому ID'
 
     @classmethod
     def get_service(cls):
@@ -120,3 +129,9 @@ class PlayList:
             total_duration += duration
         return total_duration
 
+
+broken_video = Video('broken_video_id')
+print(broken_video.title)
+# None
+print(broken_video.like_count)
+# None
